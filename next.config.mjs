4e6bbb7a5dbaf3c11f9b0e4d8e42391277a51b77
Yusuke Narita <nariyu@playwell.jp>
2024-06-10 20:11:52 +0900

    add PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import withPWA from "next-pwa";
+import packageJson from "./package.json" assert { type: "json" };
+
+const nextConfig = withPWA({ dest: "public" })({
+  env: {
+    VERSION: packageJson.version,
+    ENV: process.env.ENV,
+    VERCEL_URL: process.env.VERCEL_URL,
+    VERCEL_GIT_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA,
+  },
+});
 
 export default nextConfig;
